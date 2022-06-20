4fc77a8554b0f304aa07b02bc78b291901314ef0
Dario Turchi <iodariomdq@outlook.com>
2022-06-20 01:03:55 -0300

    first commit - fetch ok

---
+++ js/ejemplo.js
@@ -0,0 +1,4 @@
+export function mostrarEnConsola(){
+    console.log('hola mundo');
+}
+
---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+//fetch
+
+
+(()=>{
+    fetch('https://fakestoreapi.com/products')
+    .then(res=>res.json())
+    .then(data=>(console.log(data)))
+})()
+       
+
+
+
+
+
+
